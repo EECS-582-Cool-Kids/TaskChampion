b46779486001e058c4619fa31b0bd99f13223a42
Ethan <ethantberkley@gmail.com>
2025-02-05 16:23:58 -0600

    Kinda works

--- taskchampion.py
+++ taskchampion.py
@@ -3,6 +3,37 @@
 import sys
 from PySide6 import QtCore, QtWidgets
 from taskw_ng import TaskWarrior
+from typing import TypeAlias, Literal
+from utils.task import Task, status_t, priority_t
+
+# class TableRow()
+w = TaskWarrior()
+
+def hello():
+    print("hello world")
+
+class Checkbox:
+    def __init__(self, taskID : str):
+        self.task = Task(w.get_task(uuid=taskID)[1])
+        self.task_text: str = str(self.task.get_description())
+
+        self.checkbox = QtWidgets.QCheckBox(self.task_text)
+        self.checkbox.setChecked(self.task.get_status() == 'completed')
+
+        self.checkbox.stateChanged.connect(lambda: self.checkCheckbox()) # Have to pass 
+
+        
+    @QtCore.Slot()
+    def checkCheckbox(self):
+        if self.checkbox.isChecked():
+            w.task_update({"uuid": self.task.get_uuid(), "status": 'completed'})
+
+        else:
+            w.task_update({"uuid": self.task.get_uuid(), "status": 'pending'})
+
+    def linkToLayout(self, layout : QtWidgets.QVBoxLayout):
+        layout.addWidget(self.checkbox)
+
 
 class TaskChampionWidget(QtWidgets.QWidget):
     '''The main widget for the Task Champion application.'''
@@ -11,13 +42,27 @@ class TaskChampionWidget(QtWidgets.QWidget):
 
         # Initialize the layout
         self.qtLayout = QtWidgets.QVBoxLayout(self)
+    
+    def addTask(self, newTask: Task) -> None:
+        layout = QtWidgets.QHBoxLayout(self)
+        task_name = QtWidgets.QTextEdit(str(newTask.get_description()))
+        
+        task_check = Checkbox(str(newTask.get_uuid()))
+
+        # task_check_cell = QtWidgets.QTableWidgetItem()
+
+        layout.addWidget(task_check.checkbox)
+        layout.addWidget(task_name)
+        # self.table.setItem(rowNum,  1, QtWidgets.QTableWidgetItem(task_check.checkbox))
+        self.qtLayout.addLayout(layout)
+        
 
 class TaskChampionGUI:
     '''The main application class for Task Champion.'''
     def __init__(self):
         # Initialize the Qt App and TaskWarrior objects
         self.qtapp = QtWidgets.QApplication([])
-        self.warrior = TaskWarrior()
+        # self.warrior = TaskWarrior()
 
         # Initialize the main Qt Widget
         self.mainWidget = TaskChampionWidget()
@@ -32,4 +77,10 @@ class TaskChampionGUI:
 # Program entry point
 if __name__ == "__main__":
     app = TaskChampionGUI()
+    tasks = w.load_tasks()
+
+    for task in [*tasks['pending'], *tasks['completed']]:
+        app.mainWidget.addTask(Task(task))
+        
+
     sys.exit(app.onExit())
---
+++ utils/task.py
@@ -0,0 +1,75 @@
+from taskw_ng import task, fields
+from typing import TypeAlias, Literal, cast
+
+status_t: TypeAlias = Literal['pending', 'completed', 'deleted', 'waiting', 'recurring']
+priority_t: TypeAlias = Literal['H', 'M', 'L'] | None
+
+class Task(task.Task):
+    def get_annotations(self) -> fields.AnnotationArrayField:
+        return self['annotations']
+
+    def get_depends(self) -> fields.CommaSeparatedUUIDField: 
+        return self['depends']
+
+    def get_description(self) -> fields.StringField:
+        return self['description']
+
+    def get_due(self) -> fields.DateField: 
+        return self['due']
+
+    def get_end(self) -> fields.DateField:
+        return self['end']
+
+    def get_entry(self)-> fields.DateField:
+        return self['entry']
+
+    def get_id(self) -> fields.NumericField:
+        return self['id']
+
+    def get_imask(self) -> fields.NumericField:
+        return self['imask']
+
+    def get_mask(self) -> fields.StringField:
+        return self['mask']
+
+    def get_modified(self) -> fields.DateField:
+        return self['modified']
+
+    def get_parent(self) -> fields.StringField:
+        return self['parent']
+
+    def get_priority(self) -> priority_t:
+        return cast(priority_t, str(self['priority']))
+
+    def get_project(self) -> fields.StringField:
+        return self['project']
+
+    def get_recur(self) -> fields.StringField:
+        return self['recur']
+
+    def get_scheduled(self) -> fields.DateField:
+        return self['scheduled']
+
+    def get_start(self) -> fields.DateField:
+        return self['start']
+
+    def get_status(self) -> status_t:
+        return cast(status_t, str(self['status']))
+
+    def get_tags(self) -> fields.ArrayField:
+        return self['tags']
+
+    def get_until(self) -> fields.DateField:
+        return self['until']
+
+    def get_urgency(self) -> fields.NumericField:
+        return self['urgency']
+
+    def get_uuid(self) -> fields.UUIDField:
+        return self['uuid']
+
+    def get_wait(self) -> fields.DateField:
+        return self['wait']
+    
+    
+

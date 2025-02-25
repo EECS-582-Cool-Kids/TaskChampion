3195d73b1900c038e5f9ebae7d2991cb9d164840
Jacob Wilkus <jwilkus1@gmail.com>
2025-02-25 16:36:22 -0600

    add prologue comment to gui tests

--- gui-tests.py
+++ gui-tests.py
@@ -1,3 +1,20 @@
+"""
+ *  Module Name: gui-tests.py
+ *  Purpose: Unit Tests for the internal TaskChampion GUI objects.
+ *  Inputs: None
+ *  Outputs: None
+ *  Additional code sources: None
+ *  Developers: Jacob Wilkus
+ *  Date: 2/25/2025
+ *  Last Modified: 2/25/2025
+ *  Preconditions: None
+ *  Postconditions: None
+ *  Error/Exception conditions: None
+ *  Side effects: None
+ *  Invariants: None
+ *  Known Faults: None encountered
+"""
+
 class TestClass:
     def test_placeholder(self):
         pass

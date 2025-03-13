b49989964eac7288f6ee26d4fe6d9a103e125768
Jacob Wilkus <jwilkus1@gmail.com>
2025-03-13 13:57:36 -0500

    add combo box for adding previous project to new task

--- components/Dialogs/add_task_dialog.py
+++ components/Dialogs/add_task_dialog.py
@@ -17,6 +17,7 @@
 
 from PySide6 import QtWidgets
 from typing import Optional
+from utils.task_api import api
 
 class AddTaskDialog(QtWidgets.QDialog):
     class TaskDetails:
@@ -29,7 +30,6 @@ class AddTaskDialog(QtWidgets.QDialog):
             self.due = due
 
     def __init__(self):
-
         super().__init__()
 
         self.form = QtWidgets.QFormLayout()
@@ -37,7 +37,8 @@ class AddTaskDialog(QtWidgets.QDialog):
         self.description = QtWidgets.QLineEdit()
         self.tag = QtWidgets.QLineEdit()
         self.priorities = QtWidgets.QComboBox()
-        self.project = QtWidgets.QLineEdit()
+        self.projects = QtWidgets.QComboBox()
+        self.new_project = QtWidgets.QLineEdit()
         self.recurring_box = QtWidgets.QCheckBox()
 
         self.is_recurring = False
@@ -64,7 +65,8 @@ class AddTaskDialog(QtWidgets.QDialog):
         self.form.addRow("Description*", self.description)
         self.form.addRow("Tag", self.tag)
         self.form.addRow("Priority", self.priorities)
-        self.form.addRow("Project", self.project)
+        self.form.addRow("Project", self.projects)
+        self.form.addRow("New Project", self.new_project)
         self.form.addRow("Is Recurring?", self.recurring_box)
         self.form.addRow("Recurrence", self.recurrence)
         self.form.addRow("Due Date", self.due_date)
@@ -81,6 +83,15 @@ class AddTaskDialog(QtWidgets.QDialog):
         self.buttons.rejected.connect(self.reject)
 
     def add_task(self) -> Optional[TaskDetails]:
+        # Define the projects
+        self.projects.clear()
+        self.projects.addItem("New Project...")
+        
+        for task in api.task_list:
+            task_project = task.get_project()
+            if task_project != None:
+                self.projects.addItem(task_project)
+
         if self.exec():
             if not self.is_recurring:  # If the task is not recurring
                 self.recurrence = None  # Set the recurrence to None
@@ -92,9 +103,12 @@ class AddTaskDialog(QtWidgets.QDialog):
             if self.priorities.currentText() == "None":  # If the priority is None
                 self.priorities.clear()  # Clear the priority field
 
+            task_project = self.projects.currentText()
+            if self.projects.currentText() == "New Project...":
+                task_project = self.new_project.text()
 
             return AddTaskDialog.TaskDetails(self.description.text(), self.tag.text(), self.priorities.currentText(),
-                                             self.project.text(), self.recurrence, self.due_date)  # Return the task details
+                                             task_project, self.recurrence, self.due_date)  # Return the task details
         else:
             return None
         #

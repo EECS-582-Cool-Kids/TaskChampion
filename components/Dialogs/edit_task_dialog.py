3aeafa596f89a9ca3712cfb8fff6a8bd0a52287d
Richard Moser <moserrj@ku.edu>
2025-03-30 22:17:06 -0500

    comments

--- components/Dialogs/edit_task_dialog.py
+++ components/Dialogs/edit_task_dialog.py
@@ -23,9 +23,9 @@ from utils.task_api import api
 class EditTaskDialog(QtWidgets.QDialog):
     def __init__(self, delete_task, description="", due="", priority="", project="", tags=[]):
         super().__init__()
-        self.setWindowTitle("Edit Task")
-        self.form = QtWidgets.QFormLayout()
-        self.deletion_function = delete_task
+        self.setWindowTitle("Edit Task")  # Set the title of the dialog
+        self.form = QtWidgets.QFormLayout()  # Create a form layout for the dialog
+        self.deletion_function = delete_task  # Function to delete the task
 
         self.description_text = QtWidgets.QLineEdit(description) # set the description text to the description of the task
 
@@ -47,67 +47,67 @@ class EditTaskDialog(QtWidgets.QDialog):
         # OK and Cancel buttons
         button_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.StandardButton.Ok
                                       | QtWidgets.QDialogButtonBox.StandardButton.Cancel)
-        self.delete_button = QtWidgets.QPushButton("Delete Task")
-        self.delete_button.clicked.connect(self.delete)
+        self.delete_button = QtWidgets.QPushButton("Delete Task")  # Create a delete button
+        self.delete_button.clicked.connect(self.delete)  # Connect the delete button to the delete function
 
-        layout = QtWidgets.QVBoxLayout()
-        layout.addLayout(self.form)
-        layout.addWidget(button_box)
-        layout.addWidget(self.delete_button)
+        layout = QtWidgets.QVBoxLayout()  # Create a vertical layout for the dialog
+        layout.addLayout(self.form)  # Add the form layout to the vertical layout
+        layout.addWidget(button_box)  # Add the button box to the vertical layout
+        layout.addWidget(self.delete_button)  # Add the delete button to the vertical layout
 
-        self.setLayout(layout)
+        self.setLayout(layout)  # Set the layout of the dialog to the vertical layout
 
         # delete confirmation popup
-        button_box.accepted.connect(self.accept)
-        button_box.rejected.connect(self.reject)
+        button_box.accepted.connect(self.accept)  # Connect the OK button to accept the dialog
+        button_box.rejected.connect(self.reject)  # Connect the Cancel button to reject the dialog
 
         # handle projects
         # Create a line edit for the project input
-        self.projects = QtWidgets.QComboBox()
-        self.new_project = QtWidgets.QLineEdit()
+        self.projects = QtWidgets.QComboBox()  # Create a combo box for the project
+        self.new_project = QtWidgets.QLineEdit()  # Create a line edit for the new project input
 
         # Store previously used projects
         self.project_history = []  # List to store previously used projects
         self.project_list = []  # List to store currently added projects
 
         # Create "Add Project" button
-        self.add_project_button = QtWidgets.QPushButton("Add Project")
-        self.add_project_button.clicked.connect(self.add_project_to_list)
+        self.add_project_button = QtWidgets.QPushButton("Add Project")  # Create a button to add a project
+        self.add_project_button.clicked.connect(self.add_project_to_list)  # Connect the button to the add project function
 
         # Create a layout to hold the project input field and button side by side
-        self.project_layout = QtWidgets.QHBoxLayout()
-        self.project_layout.addWidget(self.add_project_button)
+        self.project_layout = QtWidgets.QHBoxLayout()  # Create a horizontal layout for the project input field and button
+        self.project_layout.addWidget(self.add_project_button)  # Add the button to the layout
 
         self.populate_project_list() # Populate the project list with existing projects
 
 
         # handle tags
         # Create a line edit for the tag input
-        self.tag = QtWidgets.QLineEdit()
-        self.tag.setPlaceholderText("Enter tag name")
+        self.tag = QtWidgets.QLineEdit()  # Create a line edit for the tag input
+        self.tag.setPlaceholderText("Enter tag name")  # Set the placeholder text for the tag input field
 
         # Store previously used tags
         self.tag_history = []  # List to store previously used tags
         self.tags_list = []  # List to store currently added tags
 
         # Create "Add Tag" button
-        self.add_tag_button = QtWidgets.QPushButton("Add Tag")
-        self.add_tag_button.clicked.connect(self.add_tag_to_list)
+        self.add_tag_button = QtWidgets.QPushButton("Add Tag")  # Create a button to add a tag
+        self.add_tag_button.clicked.connect(self.add_tag_to_list)  # Connect the button to the add tag function
 
         # Create a layout to hold the tag input field and button side by side
-        self.tag_layout = QtWidgets.QHBoxLayout()
-        self.tag_layout.addWidget(self.tag)
-        self.tag_layout.addWidget(self.add_tag_button)
-        self.tag_layout.addStretch()
+        self.tag_layout = QtWidgets.QHBoxLayout()  # Create a horizontal layout for the tag input field and button
+        self.tag_layout.addWidget(self.tag)  # Add the tag input field to the layout
+        self.tag_layout.addWidget(self.add_tag_button)  # Add the button to the layout
+        self.tag_layout.addStretch()  # Add stretch to the layout to push the button to the right
 
 
         # Create a layout to hold the added tag "bubbles"
-        self.tag_bubble_widget = QtWidgets.QWidget()
-        self.tag_bubble_layout = QtWidgets.QHBoxLayout(self.tag_bubble_widget)
-        self.tag_bubble_layout.setContentsMargins(0, 0, 0, 0)
+        self.tag_bubble_widget = QtWidgets.QWidget()  # Create a widget to hold the tag bubbles
+        self.tag_bubble_layout = QtWidgets.QHBoxLayout(self.tag_bubble_widget)  # Create a horizontal layout for the tag bubbles
+        self.tag_bubble_layout.setContentsMargins(0, 0, 0, 0)  # Set the margins of the layout to 0
         self.tag_bubble_layout.setSpacing(2)  # Space between tags
 
-        self.populate_tag_list(tags)
+        self.populate_tag_list(tags)  # Populate the tag list with existing tags
 
 
         self.tag.mousePressEvent = lambda event: self.show_tag_menu() # Connect tag input field to show menu on focus
@@ -118,13 +118,13 @@ class EditTaskDialog(QtWidgets.QDialog):
 
 
         # Add all the widgets to the form layout
-        self.form.addRow("Description", self.description_text)
-        self.form.addRow("Priority", self.priority_text)
-        self.form.addRow("Project", self.projects)
-        self.form.addRow("New Project", self.new_project)
-        self.form.addRow("Due Date", self.due_date)
-        self.form.addRow("Tags", self.tag_layout)
-        self.form.addRow(self.tag_bubble_widget)
+        self.form.addRow("Description", self.description_text)      # description
+        self.form.addRow("Priority", self.priority_text)            # priority
+        self.form.addRow("Project", self.projects)                  # project
+        self.form.addRow("New Project", self.new_project)           # new project input
+        self.form.addRow("Due Date", self.due_date)                 # due date
+        self.form.addRow("Tags", self.tag_layout)                   # tag input field and button
+        self.form.addRow(self.tag_bubble_widget)                    # tag bubbles
 
     # These properties make using this dialog a little cleaner. It's much
     # nicer to type "addDialog.address" to retrieve the address as compared
@@ -146,27 +146,27 @@ class EditTaskDialog(QtWidgets.QDialog):
     @property
     def project(self):
         if self.projects.currentText() == "New Project...":
-            return self.new_project.text()
+            return self.new_project.text()  # If the user selects "New Project...", return the text from the new project input field
         if self.projects.currentText() == "":
-            return None
-        return self.projects.currentText()
+            return None  # If the user doesn't select a project, return None
+        return self.projects.currentText()  # Return the selected project from the combo box
 
     def tags(self):
-        return self.tags_list
+        return self.tags_list  # Return the list of tags
 
     def delete(self):
         response = QtWidgets.QMessageBox.question(self, "Delete Task", "Are you sure you want to delete this task?",
                                                   QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No)
 
         if response == QtWidgets.QMessageBox.StandardButton.Yes: # If the user clicks Yes, the task will be deleted.
-            self.deletion_function()
-            self.accept()
+            self.deletion_function()  # Call the deletion function passed to the dialog
+            self.accept()  # Accept the dialog to close it
 
         else: # If the user doesn't want to delete the task, then the dialog will close.
-            return
+            return  # Return to close the dialog
 
     def populate_project_list(self):
-        project_history = []
+        project_history = []  # List to store previously used projects
         for task in api.task_list:  # Iterate through the task list
             task_project = task.get_project()  # Get the project of the task
             if task_project is not None and not task_project in project_history:  # If the project is not already in the list
